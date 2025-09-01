cae93a565029590c46c03e61ef581614e4cf039b
boodovskiy <dzak1987@mail.ru>
2025-09-01 08:51:10 +0300

    Setup fonts, fix images types

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+      <Text className="my-10 text-3xl font-rubik-bold">Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,24 @@ module.exports = {
         "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
         "rubik-light": ["Rubik-Light", "sans-serif"],
       },
+      colors: {
+        primary: {
+          100: "#0061FF0A",
+          200: "#0061FF1A",
+          300: "#0061FF2A",
+        },
+        accent: {
+          100: "#FBFBFD",
+          200: "#FF5E3A1A",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
+      },
     },
   },
   plugins: [],
