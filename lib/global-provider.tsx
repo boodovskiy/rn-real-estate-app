aacc3dfa4e63f65db81d7dab293c72e816cd6184
boodovskiy <dzak1987@mail.ru>
2025-09-06 13:40:52 +0300

    Add Global Provider wrapper

---
+++ lib/global-provider.tsx
@@ -0,0 +1,21 @@
+import { createContext, ReactNode } from "react";
+
+interface User {
+  $id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+interface GlobalContextType {
+  isLoggedIn: boolean;
+  user: User | null;
+  loading: boolean;
+  refetch: (newParams?: Record<string, string | number>) => Promise<void>;
+}
+
+const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
+
+export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+};
