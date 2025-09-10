338028e9c5f11cdefde1008583ccca874b5271b8
boodovskiy <dzak1987@mail.ru>
2025-09-10 11:15:08 +0300

    Implement Tabs Navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -11,10 +10,6 @@ export default function Index() {
       }}
     >
       <Text className="my-10 text-3xl font-rubik-bold">Welcome to ReState</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
