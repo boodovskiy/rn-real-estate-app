c67e4c8130d55c129beb3681cda67c52bf989808
boodovskiy <dzak1987@mail.ru>
2025-09-22 08:59:08 +0300

    Fix issue with duplicated keyExtractor

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -51,7 +51,7 @@ export default function Index() {
         renderItem={({ item }) => (
           <Card item={item} onPress={() => handleCardPress(item.$id)} />
         )}
-        keyExtractor={(item) => item.toString()}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
@@ -95,7 +95,7 @@ export default function Index() {
                     onPress={() => handleCardPress(item.$id)}
                   />
                 )}
-                keyExtractor={(item) => item.toString()}
+                keyExtractor={(item) => item.$id}
                 horizontal
                 bounces={false}
                 showsHorizontalScrollIndicator={false}
