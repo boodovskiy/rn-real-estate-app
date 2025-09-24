4875d1ff7e5d99ddd44847699f6d5c3d8d471834
boodovskiy <dzak1987@mail.ru>
2025-09-24 08:43:34 +0300

    Implement Explore Screen

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
+import Search from "@/components/Search";
+import { getProperties } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Explore = () => {
-  return (
-    <View>
-      <Text>Explore</Text>
-    </View>
-  )
-}
+  const insets = useSafeAreaInsets();
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
 
-export default Explore
+  const {
+    data: properties,
+    loading,
+    refetch,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 20,
+    },
+    skip: true,
+  });
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 20,
+    });
+  }, [params.filter, params.query]);
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+  return (
+    <View className="bg-white h-full" style={{ paddingTop: insets.top }}>
+      <FlatList
+        data={properties}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : (
+            <NoResults />
+          )
+        }
+        ListHeaderComponent={
+          <View className="px-5">
+            <Search />
+
+            <View className="mt-5 ">
+              <Filters />
+              <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+                Found {properties?.length || 0} Properties
+              </Text>
+            </View>
+          </View>
+        }
+      />
+    </View>
+  );
+};
+
+export default Explore;
