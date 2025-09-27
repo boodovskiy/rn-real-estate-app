b8ab6e3bc98de9020b800b11867912c200a62c4e
boodovskiy <dzak1987@mail.ru>
2025-09-27 10:23:30 +0300

    Add header and image on Property Details page

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,12 +1,70 @@
-import { useLocalSearchParams } from "expo-router";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { getPropertyById } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const Property = () => {
-  const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams<{ id?: string }>();
+
+  const windowHeight = Dimensions.get("window").height;
+
+  const { data: property } = useAppwrite({
+    fn: getPropertyById,
+    params: { id: id! },
+  });
+
   return (
     <View>
-      <Text>Property {id}</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 bg-white"
+      >
+        <View className="relative w-full" style={{ height: windowHeight / 2 }}>
+          <Image
+            source={{ uri: property?.image }}
+            className="size-full"
+            resizeMode="cover"
+          />
+
+          <Image
+            source={images.whiteGradient}
+            className="absolute top-0 w-full z-40"
+          />
+
+          <View
+            className="z-50 absolute inset-x-7"
+            style={{ top: Platform.OS === "ios" ? 70 : 20 }}
+          >
+            <View className="flex flex-row items-center w-full justify-between">
+              <TouchableOpacity
+                className="flex flex-row bg-primary-200 founded-full size-11 items-center justify-center"
+                onPress={() => router.back()}
+              >
+                <Image source={icons.backArrow} className="size-5" />
+              </TouchableOpacity>
+
+              <View className="flex flex-row items-center gap-3">
+                <Image
+                  source={icons.heart}
+                  className="size-7"
+                  tintColor={"#191D31"}
+                />
+                <Image source={icons.send} className="size-7" />
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
