3d461bcf50b87628180f0e9ce4ea793dd7d58b08
boodovskiy <dzak1987@mail.ru>
2025-09-29 08:33:23 +0300

    Add Facilities section

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,3 +1,4 @@
+import { facilities } from "@/constants/data";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { getPropertyById } from "@/lib/appwrite";
@@ -141,6 +142,44 @@ const Property = () => {
             <Text className="text-black-200 text-base font-rubik mt-2">
               {property?.description}
             </Text>
+          </View>
+
+          <View className="mt-7">
+            <Text className="text-black-300 text-xl font-rubik-bold">
+              Facilities
+            </Text>
+
+            {property?.facilities.length > 0 && (
+              <View className="flex flex-row  flex-wrap items-start justify-start mt-2 gap-5">
+                {property?.facilities.map((item: string, index: number) => {
+                  const facility = facilities.find(
+                    (facility) => facility.title === item
+                  );
+
+                  return (
+                    <View
+                      key={index}
+                      className="flex flex-1 flex-col items-center min-w-16 max-w-20"
+                    >
+                      <View className="size-14 bg-primary-100 founded-full flex items-center justify-center">
+                        <Image
+                          source={facility ? facility.icon : icons.info}
+                          className="size-6"
+                        />
+                      </View>
+
+                      <Text
+                        className="text-black-300 text-sm text-center font-rubik mt-1.5"
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                      >
+                        {item}
+                      </Text>
+                    </View>
+                  );
+                })}
+              </View>
+            )}
           </View>
         </View>
       </ScrollView>
