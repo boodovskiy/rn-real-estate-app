5db676ee83d0a92fe4db8071305d97e7bb01b242
boodovskiy <dzak1987@mail.ru>
2025-10-06 09:04:50 +0300

    Add Book Now button for property screen

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -251,6 +251,28 @@ const Property = () => {
           )}
         </View>
       </ScrollView>
+
+      <View className="absolute bg-white bottom-0 w-full rounded-t-2xl border-t border-r border-l border-primary-200 p-7">
+        <View className="flex flex-row items-center justify-between gap-10">
+          <View className="flex flex-col items-start">
+            <Text className="text-black-200 text-xs font-rubik-medium">
+              Price
+            </Text>
+            <Text
+              className="text-primary-300 text-start text-2xl font-rubik-bold"
+              numberOfLines={1}
+            >
+              ${property?.price}
+            </Text>
+          </View>
+
+          <TouchableOpacity className="flex-1 flex flex-row items-center justify-center bg-primary-300 py-3 rounded-full shadow-md shadow-zinc-400">
+            <Text className="text-white text-lg text-center text-rubik-bold">
+              Book Now
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
